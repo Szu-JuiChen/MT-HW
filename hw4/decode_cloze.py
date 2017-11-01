decaf9ff6c7d96663a489e9228045c3895ebe242
Szu-JuiChen <0217sray@gmail.com>
2017-10-31 23:03:52 -0400

    .

--- hw4/decode_cloze.py
+++ hw4/decode_cloze.py
@@ -4,6 +4,7 @@ import utils.rand
 import argparse
 import dill
 import logging
+import pdb
 
 import torch
 from torch import cuda
@@ -20,6 +21,7 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
 BLK = "<blank>"
+BLKend = "<blank>\n"
 
 def greedy(model, seq, vocab):
     seq_prob = model.forward(Variable(seq[:, None]))[:,0,:]
@@ -27,9 +29,10 @@ def greedy(model, seq, vocab):
     new_seq.copy_(seq)
     
     for i in range(seq.size(0)):
-        if vocab.itos[seq[i]] == BLK:
+        if vocab.itos[seq[i]] == BLK or vocab.itos[seq[i]] == BLKend:
             _, pred_idx = torch.max(seq_prob[i], dim=0)
             pred_idx = pred_idx.data[0]
+            
             new_seq[i] = pred_idx
     return new_seq
 
@@ -51,6 +54,7 @@ def main(options):
     new_seqs = []
     for seq in test:
         new_seq = greedy(model, seq, vocab)
+        
         #new_string = tensor2string(new_seq, vocab)
         #strings.append(new_string)
         new_seqs.append(new_seq)
@@ -63,17 +67,20 @@ def main(options):
 
     with open(options.output_file, 'w+') as f_w, open(options.test_file, 'r') as f_r:
         seq_index = 0
+        Ans=[]
         for test_seq in f_r:
+            Ans.append([])
             test_seq_split = test_seq.split(' ')
             for index, word in enumerate(test_seq_split):
-                if word == "<blank>":
-                    test_seq_split[index] = vocab.itos[new_seqs[seq_index][index + 1]]
-            new_seq = u' '.join(test_seq_split).encode('utf-8')
+                if word == BLK or word == BLKend:
+                   test_seq_split[index] = vocab.itos[new_seqs[seq_index][index + 1]] # due to there are " ' " in the begin and end
+                   Ans[-1].append(vocab.itos[new_seqs[seq_index][index + 1]])
+            new_seq = u' '.join(Ans[-1]).encode('utf-8')
+            new_seq += '\n'
             f_w.write(new_seq)
+            
             seq_index += 1
-
-
-
+        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Decoding part of mt hw 4")
     parser.add_argument("--data_file", required=True,
