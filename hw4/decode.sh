9d8984b9075f1a14ed6087c28f94252510896c32
Chen Szu-Jui <cszu@b01.clsp.jhu.edu>
2017-11-01 13:08:16 -0400

    .

--- hw4/decode.sh
+++ hw4/decode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source activate NeuralHawkes
+source activate torch
 
-python decode_cloze.py --data_file data/hw4_data.bin --model_file experiment/model.nll_5.18.epoch_6 --output_file output_test --test_file data/test.en.txt.cloze
+python decode_cloze.py --data_file data/hw4_data.bin --model_file model.nll_5.11.epoch_4 --output_file output_test --test_file data/test.en.txt.cloze
--- hw4/train_uni.sh
+++ hw4/train_uni.sh
@@ -12,8 +12,8 @@ LIBRARY_PATH=/export/b18/xma/libs/cudnn-6/cuda/lib64
 code_path="/export/b03/cszu/MT-HW/hw4"
 
 #mkdir -p model
-CUDA_VISIBLE_DEVICES=${device} python ${code_path}/train_bi.py \
-  --optimizer Adam -lr 1e-2 --batch_size 48 \
+CUDA_VISIBLE_DEVICES=${device} python ${code_path}/train.py \
+  --optimizer Adam -lr 1e-2 --batch_size 48 --estop 1e-3\
   --gpuid 0 \
   --data_file ${code_path}/data/hw4_data.bin \
   --model_file model
