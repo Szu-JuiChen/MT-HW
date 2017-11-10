9f7f028fbe70f67d4800ae9b0492b8348086e3a5
Chen Szu-Jui <cszu@b05.clsp.jhu.edu>
2017-11-09 21:21:06 -0500

    .

--- hw4/model.py
+++ hw4/model.py
@@ -360,17 +360,6 @@ class RNNLM(nn.Module):
 
 
 
-from torch.autograd import Variable
-
-# TODO: Your implementation goes here
-class RNNLM(nn.Module):
-  def __init__(self, vocab_size):
-    super(RNNLM, self).__init__()
-    pass
-
-  def forward(self, input_batch):
-    pass
-
 
 # TODO: Your implementation goes here
 class BiRNNLM(nn.Module):
